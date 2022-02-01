311f65af0cbb334c0ccd054b25d7ba7b4be104e7
Alex Rutar <alex@rutar.org>
2022-02-01 02:39:15 +0000

    fix typo in completions

--- completions/vs.fish
+++ completions/vs.fish
@@ -7,7 +7,7 @@ complete --command vs --exclusive --condition __fish_use_subcommand --arguments 
 complete --command vs --exclusive --condition __fish_use_subcommand --arguments list --description "List available session files"
 complete --command vs --exclusive --condition __fish_use_subcommand --arguments init --description "Start up a new session"
 
-complete --command vs --exclusive --arguments "(v list)" \
+complete --command vs --exclusive --arguments "(vs list)" \
     --condition "__fish_seen_subcommand_from open delete"
-complete --command vs --exclusive --arguments "(v _list_all)" \
+complete --command vs --exclusive --arguments "(vs _list_all)" \
     --condition "__fish_seen_subcommand_from init rename"
