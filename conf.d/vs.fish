bd4882a953a33a3f699e4eb605e89aa5cd10c3af
Alex Rutar <alex@rutar.org>
2022-02-01 02:38:14 +0000

    add install hook to check for dependencies

--- conf.d/vs.fish
+++ conf.d/vs.fish
@@ -1,3 +1,12 @@
+function _vs_install --on-event vs_install
+    if not which fd
+        echo "Warning: cannot find command 'fd'. See https://github.com/alexrutar/vs#dependencies for more details."
+    end
+    if not which fzf
+        echo "Warning: cannot find command 'fzf'. See https://github.com/alexrutar/vs#dependencies for more details."
+    end
+end
+
 function _vs_uninstall --on-event vs_uninstall
     functions --erase vs
     functions --erase __vs_list_sessions
